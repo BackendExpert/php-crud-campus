dc07e79b92f35f257dcae6833ff7db8c433c88d7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 16:08:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP CRUD</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.0/src/jkcss.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss-icons@1.2.0/jkcss/jkicons.min.css" rel="stylesheet">
-
-    <script src="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.0/js/jkcss.min.js"></script>
+    <link rel="stylesheet" href="https://jkcss-css-framework.github.io/JKCSS-Framework/jkcss/jkcss.min.css">
+    <script src="https://jkcss-css-framework.github.io/JKCSS-Framework/js/jkcss.min.js"></script>
 </head>
 <body>
+
--- lib/views/crud.php
+++ lib/views/crud.php
@@ -5,7 +5,24 @@
 <link rel="stylesheet" href="../../css/app.min.css">
 
 <div class="site-container">
-    
+    <button class="jkbtn jkbtn-blue" id="jkbtn-popup">Click Here</button>
+
+    <div class="jk-modal" id= "modalJK">
+        <div class="jk-modal-content">
+            <div class="jk-mheader">
+                <h1>JKCSS Modal</h1>
+            <div>
+            <hr>
+            <div class="jk-mbody">
+                <p>A CSS Framework<p>
+                <p>JKCSS is a one of Framework<p>
+            <div>
+            <hr>
+            <div class="jk-mfooter">
+                <button class="jkbtn jkbtn-green" id="closeModel">Close</button>
+            <div>
+        <div>
+    </div>
 </div>
 
-<script src="../../js/app.min.js"></script>
+
