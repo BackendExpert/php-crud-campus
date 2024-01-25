82a854fca910aad7da83ed05058ecd06905c68ba
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 16:22:59 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/crud.php
+++ lib/views/crud.php
@@ -11,14 +11,28 @@
         <hr>
         <div class="jk-mbody">
             <form action="" method="POST">
+                <label for="nic">NIC : </label>
+                <input type="text" id="basicForm" name="nic" class="control-jkforms">
+
                 <label for="username">Username : </label>
                 <input type="text" id="basicForm" name="username" class="control-jkforms">
-            </form>
+
+                <label for="email">Email : </label>
+                <input type="email" id="basicForm" name="email" class="control-jkforms">
+
+                <label for="Mobile">Mobile : </label>
+                <input type="text" id="basicForm" name="mobile" class="control-jkforms">
+
+                <label for="dob">Date Of Birth : </label>
+                <input type="date" id="basicForm" name="dob" class="control-jkforms">            
         <div>
         <hr>
         <div class="jk-mfooter">
-            <button class="jkbtn jkbtn-green" id="closeModel">Close</button>
+            <button class="jkbtn jkbtn-red" id="closeModel">Close</button>
+            <input type="reset" value="Clear" class="jkbtn jkbtn-gray">
+            <input type="submit" value="Submit" class="jkbtn jkbtn-green" id="closeModel" name="addUser">
         <div>
+        </form>
     <div>
 </div>
 </div>
