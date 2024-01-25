ef0175354a8ae1e4cce3d88cc5ed95b0a97dc66e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 15:55:24 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP CRUD</title>
-    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.0/src/jkcss.min.css">
+
+    <script src="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.0/js/jkcss.min.js"></script>
 </head>
 <body>
