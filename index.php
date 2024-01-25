749fe186727a87eae8a6d4db546a4066d1953035
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 15:56:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php include("lib/function/function.php"); ?>
 <link rel="stylesheet" href="css/app.min.css">
 
+<div class="site-container" style="text-align: center;">
+    <h1 class="jk-display-4">JK Display 4</h1>
+</div>
 
 <script src="js/app.min.js"></script>
