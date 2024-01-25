cbff620a43852930a462edf27628a49cdedefc63
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 15:59:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
-<?php include("lib/function/function.php"); ?>
+<?php include("lib/layouts/header.php"); ?>
 <link rel="stylesheet" href="css/app.min.css">
 
+
 <div class="site-container" style="text-align: center;">
-    <h1 class="jk-display-4">JK Display 4</h1>
+    <h1 class="jk-display-4">PHP CRUD</h1>
+    <hr>
+    <br>
+    <a href=""><button class="jkbtn jkbtn-blue">CRUD Application</button></a>
 </div>
 
 <script src="js/app.min.js"></script>
