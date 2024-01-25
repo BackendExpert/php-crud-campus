6ab20892f506aa8ef2b8e7c74eea115ffc16ea54
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-25 16:02:40 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP CRUD</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.0/src/jkcss.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss-icons@1.2.0/jkcss/jkicons.min.css" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.0/js/jkcss.min.js"></script>
 </head>
---
+++ lib/views/crud.php
@@ -0,0 +1,11 @@
+<?php 
+    include("../layouts/header.php"); 
+    include("../function/function.php"); 
+?>
+<link rel="stylesheet" href="../../css/app.min.css">
+
+<div class="site-container">
+    
+</div>
+
+<script src="../../js/app.min.js"></script>
