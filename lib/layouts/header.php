9d76849479a3b2ae86f3be129ffb20a233b18ac1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-26 23:02:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP CRUD</title>
+    <title>Document</title>
     <link rel="stylesheet" href="https://jkcss-css-framework.github.io/JKCSS-Framework/jkcss/jkcss.min.css">
-
+    <script src="https://jkcss-css-framework.github.io/JKCSS-Framework/js/jkcss.min.js"></script>
 </head>
 <body>
