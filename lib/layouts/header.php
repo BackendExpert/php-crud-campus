4532504f2d9133e567ca18b00120fd9b403c0125
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-27 20:18:38 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.1/src/jkcss.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.0.1/js/jkcss.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.1.0/src/jkcss.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@jehankandy/jkcss@4.1.0/js/jkcss.min.js"></script>
 </head>
 <body>
 
