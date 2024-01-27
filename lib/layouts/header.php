eb007b5ea378c087c76cbf455c42c5b28dce79cb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-27 07:34:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/layouts/header.php
+++ lib/layouts/header.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://jkcss-css-framework.github.io/JKCSS-Framework/jkcss/jkcss.min.css">
-    <script src="https://jkcss-css-framework.github.io/JKCSS-Framework/js/jkcss.min.js"></script>
+    <!-- <link rel="stylesheet" href="https://jkcss-css-framework.github.io/JKCSS-Framework/jkcss/jkcss.min.css"> -->
+
 </head>
 <body>
+
+<!-- <script src="https://jkcss-css-framework.github.io/JKCSS-Framework/js/jkcss.min.js"></script> -->
+
+<script src="../../js/app.js"></script>
--- lib/views/crud.php
+++ lib/views/crud.php
@@ -1,42 +1,65 @@
 <?php include("../layouts/header.php"); ?>
 
+<link rel="stylesheet" href="../../css/app.scss">
 <div class="site-container">
-<button class="jkbtn jkbtn-blue" id="jkbtnPopup">Click Here</button>
 
-<div class="jk-modal" id="modalJK">
-    <div class="jk-modal-content">
-        <div class="jk-mheader">
-            <h1>Add New User</h1>
+
+    <!-- <button class="jkbtn jkbtn-blue" id="jkbtnPopup">Click Here</button>
+
+    <div class="jk-modal" id="modalJK">
+        <div class="jk-modal-content">
+            <div class="jk-mheader">
+                <h1>Add New User</h1>
+            <div>
+            <hr>
+            <div class="jk-mbody">
+                <form action="" method="post">
+                    <label for="username">Username : </label>
+                    <input type="text" name="username" id="" class="control-jkforms">
+
+                    <label for="username">NIC : </label>
+                    <input type="text" name="nic" id="" class="control-jkforms">
+
+                    <label for="username">Email : </label>
+                    <input type="email" name="email" id="" class="control-jkforms">
+
+                    <label for="username">Name : </label>
+                    <input type="text" name="uname" id="" class="control-jkforms">
+
+                    <label for="username">Mobile : </label>
+                    <input type="text" name="mobile" id="" class="control-jkforms">
+
+                    <label for="username">Date of Birth : </label>
+                    <input type="date" name="dob" id="" class="control-jkforms">     
+                    
+                    <input type="reset" value="Clear" class="jkbtn jkbtn-gray">
+                    <input type="submit" value="Submit" name="addUser" class="jkbtn jkbtn-green">
+                </form>
+            <div>
         <div>
-        <hr>
-        <div class="jk-mbody">
-            <form action="" method="post">
-                <label for="username">Username : </label>
-                <input type="text" name="username" id="" class="control-jkforms">
-
-                <label for="username">NIC : </label>
-                <input type="text" name="nic" id="" class="control-jkforms">
-
-                <label for="username">Email : </label>
-                <input type="email" name="email" id="" class="control-jkforms">
-
-                <label for="username">Name : </label>
-                <input type="text" name="uname" id="" class="control-jkforms">
-
-                <label for="username">Mobile : </label>
-                <input type="text" name="mobile" id="" class="control-jkforms">
-
-                <label for="username">Date of Birth : </label>
-                <input type="date" name="dob" id="" class="control-jkforms">
-            
+    </div>
+</div> -->
         <div>
-        <hr>
-        <div class="jk-mfooter">
-            <button class="jkbtn jkbtn-green" id="closeModel">Close</button>
-            <input type="reset" value="Clear" class="jkbtn jkbtn-gray">
-            </form>
-        <div>
-    <div>
-</div>
-</div>
+        <button class="jkbtn jkbtn-blue" id="jkbtnPopup">Click Here</button>
 
+
+            <div class="jk-modal" id="modalJK">
+                <div class="jk-mheader">
+                    <h1>JKCSS Modal</h1>
+                <div>
+                <hr>
+                <div class="jk-mbody">
+                    <p>A CSS Framework<p>
+                    <p>JKCSS is a one of Framework<p>
+                <div>
+                <hr>
+                <div class="jk-mfooter">
+                    <button class="jkbtn jkbtn-green" id="closeModel">Close</button>
+                <div>
+            <div>
+        </div>
+
+
+        <div class="code-back">
+            sdakjlk
+        </div>
