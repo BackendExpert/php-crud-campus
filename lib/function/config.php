17dffb6a49a9159e3a2de79668eea320eb03eeb9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-27 20:21:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/function/config.php
+++ lib/function/config.php
@@ -5,7 +5,10 @@
         $pass = "";
         $db = "php-crud-campus";
    
-        
+        $con = mysqli_connect($server,$user,$pass,$db);
+
+        $result = (!$con)?"Connection Lost":$con;
+        return $result;
     }
 
 
