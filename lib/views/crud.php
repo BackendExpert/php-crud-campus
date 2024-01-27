2f04db4e8fcf6b9337db31631ab905d90092eb8b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-27 18:16:37 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/crud.php
+++ lib/views/crud.php
@@ -10,7 +10,22 @@
             <h1 class="jk-display-6">Add New Member</h1>
             <form action="" method="post">
                 <label for="username">Username : </label>
-                <input type="text" name="" id="" class="control-jkforms inputs-crtl">
+                <input type="text" name="username" id="" class="control-jkforms"><br>
+
+                <label for="email">Email : </label>
+                <input type="email" name="email" id="" class="control-jkforms"><br>
+
+                <label for="name">Name : </label>
+                <input type="text" name="uname" id="" class="control-jkforms"><br>
+                
+                <label for="mobile">Mobile : </label>
+                <input type="text" name="mobile" id="" class="control-jkforms"><br>
+                
+                <label for="nic">NIC : </label>
+                <input type="text" name="nic" id="" class="control-jkforms"><br>
+
+                <label for="birth">Date of Birth : </label>
+                <input type="date" name="dob" id="" class="control-jkforms"><br>
             </form>
             <button class="jkbtn jkbtn-red close">Close</button>
         </div>
