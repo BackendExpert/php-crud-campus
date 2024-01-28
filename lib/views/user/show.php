a5d81ae85b11fbc9d145d3e60b05091b59195213
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-01-28 20:24:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/user/show.php
+++ lib/views/user/show.php
@@ -3,9 +3,5 @@
 
 <link rel="stylesheet" href="../../../css/app.scss">
 <div class="site-container">
-    <?php 
-        $id = $_GET['id'];
-        echo $id;
-    ?>
     <?php showUser() ?>
 </div>
