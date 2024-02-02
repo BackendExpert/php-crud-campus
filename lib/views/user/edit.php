476c7c3416691136fb9285f42b20cd11a9b6c237
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-02 21:52:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- lib/views/user/edit.php
+++ lib/views/user/edit.php
@@ -2,6 +2,14 @@
 <?php include("../../function/function.php"); ?>
 
 <link rel="stylesheet" href="../../../css/app.scss">
+
+<?php 
+    if(){
+        $result = update_data();
+        echo $result;
+    }
+?>
+
 <div class="site-container">
     <?php UpdateView() ?>
 </div>
